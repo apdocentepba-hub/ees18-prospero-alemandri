1549736a1dc095a411d2f1d90df39359695d9380
apdocentepba-hub <apdocentepba@gmail.com>
2026-08-31 22:13:45 -0300

    Point reservation frontend to production Web App

--- assets/js/reservas-config.js
+++ assets/js/reservas-config.js
@@ -1,1 +1,1 @@
-window.EES18_RESERVAS_API_URL = 'https://script.google.com/macros/s/AKfycbyxFoBc3I3tpVNKB-G2vfIxJE3qvr-6TM2LdrXcLaOQ0-ZInGnfcmTAcWujVvrmIUib/exec';
+window.EES18_RESERVAS_API_URL = 'https://script.google.com/macros/s/AKfycby6mw5IqQxNAMpFqj9EMxj_z51siD9hBRG_ZGbSUIOwEKy3BQCqjOTLg8oHq2IQlFR0/exec';
