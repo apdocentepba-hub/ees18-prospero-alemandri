b2c8b4618f6d21610a0921dcc682ae9fb1d35e2c
apdocentepba-hub <apdocentepba@gmail.com>
2026-09-07 17:05:52 -0300

    chore: connect novedades production endpoint

--- assets/js/novedades-config.js
+++ assets/js/novedades-config.js
@@ -1,1 +1,1 @@
-window.EES18_NOVEDADES_API_URL = '';
+window.EES18_NOVEDADES_API_URL = 'https://script.google.com/macros/s/AKfycbzzoUz2z2CwcCtVO20bmF9BycKwdgMNUcK14TaoywcMWUJozM93YZfKyKq0LZX0osNvmA/exec';
