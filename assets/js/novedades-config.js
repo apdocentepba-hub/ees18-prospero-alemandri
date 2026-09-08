89d6704a4cc5d78cbfc41e6e092e9135fc60c74a
apdocentepba-hub <apdocentepba@gmail.com>
2026-09-07 21:53:36 -0300

    fix: use public novedades web app deployment

--- assets/js/novedades-config.js
+++ assets/js/novedades-config.js
@@ -1,1 +1,1 @@
-window.EES18_NOVEDADES_API_URL = 'https://script.google.com/macros/s/AKfycbzzoUz2z2CwcCtVO20bmF9BycKwdgMNUcK14TaoywcMWUJozM93YZfKyKq0LZX0osNvmA/exec';
+window.EES18_NOVEDADES_API_URL = 'https://script.google.com/macros/s/AKfycbzguaQvZVJnkptjrEfIi6H0BpyrUt1hb10tFBXzx_PQKdqWXZxUex1rzVMEKhr5gmZi-w/exec';
